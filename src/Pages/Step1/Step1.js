e734475f6e61333b67a90aaf63c99f031b789632
MiguelP <j.patrocinio.staiy@gmail.com>
2021-05-01 17:31:04 +0100

    WIP Step1 / created the MainContainer component

--- src/Pages/Step1/Step1.js
+++ src/Pages/Step1/Step1.js
@@ -1,14 +1,44 @@
 // Third-party imports
 import React from "react";
+import { useForm } from "react-hook-form";
+import { useHistory } from "react-router-dom";
 
 // Global imports
+import MainContainer from "../../components/MainContainer/MainContainer";
 
 // Local imports
 
 ////////////////////////////////////////////////////////////////////////////////
 
 const Step1 = () => {
-  return null;
+  // Hooks
+  const { register, handleSubmit, errors } = useForm();
+  const history = useHistory();
+
+  const onSubmit = (data) => {
+    history.push("./step2");
+  };
+
+  return (
+    <MainContainer>
+      <h2>Step 2</h2>
+      <form>
+        <input
+          //   ref={register}
+          name="firstName"
+          type="text"
+          placeholder="First Name"
+        />
+        <input
+          //   ref={register}
+          name="LastName"
+          type="text"
+          placeholder="Last Name"
+        />
+        <button type="submit">Next</button>
+      </form>
+    </MainContainer>
+  );
 };
 
 export default Step1;
