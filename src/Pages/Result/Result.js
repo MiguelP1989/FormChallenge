db151508b1471f3f3603315101f63ed499f89a81
MiguelP <j.patrocinio.staiy@gmail.com>
2021-05-10 11:51:54 +0200

    WIP Results / Displied Files selected

--- src/Pages/Result/Result.js
+++ src/Pages/Result/Result.js
@@ -8,6 +8,11 @@ import {
   TableRow,
   TableCell,
   TableBody,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  makeStyles,
 } from "@material-ui/core";
 import InsertDriveFile from "@material-ui/icons/InsertDriveFile";
 
@@ -21,8 +26,11 @@ import { useData } from "../../Context/DataContext";
 
 const Result = () => {
   // Hooks
+  const classes = useStyles();
   const { data } = useData();
-  const entries = Object.entries(data).filter((entry) => entry[0] !== "files");
+  const entries = Object.entries(data).filter((entry) => {
+    return entry[0] !== "files" && entry[1] !== undefined;
+  });
 
   // variables
   const { files } = data;
@@ -32,26 +40,54 @@ const Result = () => {
       <Typography component="h2" variant="h5">
         📋 Form values
       </Typography>
-      <TableContainer>
-        <Table>
+      <TableContainer className={classes.root}>
+        <Table className={classes.table}>
           <TableHead>
             <TableRow>
-              <TableCell>Field</TableCell>
-              <TableCell align="right">Value</TableCell>
+              <TableCell style={{ fontWeight: "bold" }}>Field</TableCell>
+              <TableCell style={{ fontWeight: "bold" }} align="right">
+                Value
+              </TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
             {entries.map((entry) => (
               <TableRow key={entry[0]}>
                 <TableCell>{entry[0]}</TableCell>
-                <TableCell align="right">{entry[1]}</TableCell>
+                <TableCell align="right">{entry[1].toString()}</TableCell>
               </TableRow>
             ))}
           </TableBody>
         </Table>
       </TableContainer>
+
+      <List>
+        {files &&
+          files.map((f, index) => (
+            <>
+              <Typography component="h2" variant="h5" align="center">
+                📦 Files
+              </Typography>
+              <ListItem key={index}>
+                <ListItemIcon>
+                  <InsertDriveFile />
+                </ListItemIcon>
+                <ListItemText primary={f.name} secondary={f.size} />
+              </ListItem>
+            </>
+          ))}
+      </List>
     </MainContainer>
   );
 };
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    marginBottom: "30px",
+  },
+  table: {
+    marginBottom: "30px",
+  },
+}));
 
 export default Result;
